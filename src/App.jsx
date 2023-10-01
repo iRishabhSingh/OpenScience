b11d6495b69d826c9a81708cb4c6626d645ccd00
Hrishabh Singh <rishab18011@gmail.com>
2023-10-01 15:36:11 +0530

    SignUp Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/signup" component={Signup} />
+      </Switch>
+    </Router>
   );
 }
 
