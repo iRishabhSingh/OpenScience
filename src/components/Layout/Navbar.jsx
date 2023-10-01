8e1d1a528e95d44ca805a427df4017058ca568ca
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-10-02 00:10:38 +0530

    navbar fix

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link, NavLink, useNavigate } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <header class="bg-[#B7EFFB]">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <header className="bg-[#B7EFFB]">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <Link
             href="/"
-            class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -16,28 +16,28 @@ const Navbar = () => {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+              className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl">Tailblocks</span>
+            <span className="ml-3 text-xl">Tailblocks</span>
           </Link>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/" class="mr-5 hover:text-gray-900">
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <Link href="/" className="mr-5 hover:text-gray-900">
               Home
             </Link>
-            <Link href="/aboutus" class="mr-5 hover:text-gray-900">
+            <Link href="/aboutus" className="mr-5 hover:text-gray-900">
               About Us
             </Link>
-            <Link href="/projects" class="mr-5 hover:text-gray-900">
+            <Link href="/projects" className="mr-5 hover:text-gray-900">
               Projects
             </Link>
-            <Link href="/dashboard" class="mr-5 hover:text-gray-900">
+            <Link href="/dashboard" className="mr-5 hover:text-gray-900">
               Dashboard
             </Link>
           </nav>
-          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
             <svg
               fill="none"
@@ -45,7 +45,7 @@ const Navbar = () => {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-4 h-4 ml-1"
+              className="w-4 h-4 ml-1"
               viewBox="0 0 24 24"
             >
               <path d="M5 12h14M12 5l7 7-7 7"></path>
