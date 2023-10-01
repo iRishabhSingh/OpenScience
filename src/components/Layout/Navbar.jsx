3ae6517c2ed0de995dad1eb0c996fb547b9a7890
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-10-02 01:34:07 +0530

    minor bug fix

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -5,8 +5,8 @@ const Navbar = () => {
     <>
       <header className="bg-[#B7EFFB]">
         <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <Link
-            href="/"
+          <NavLink
+            to="/"
             className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           >
             <svg
@@ -22,20 +22,20 @@ const Navbar = () => {
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
             <span className="ml-3 text-xl">Tailblocks</span>
-          </Link>
+          </NavLink>
           <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <Link href="/" className="mr-5 hover:text-gray-900">
+            <NavLink to="/" className="mr-5 hover:text-gray-900">
               Home
-            </Link>
-            <Link href="/aboutus" className="mr-5 hover:text-gray-900">
+            </NavLink>
+            <NavLink to="/aboutus" className="mr-5 hover:text-gray-900">
               About Us
-            </Link>
-            <Link href="/projects" className="mr-5 hover:text-gray-900">
+            </NavLink>
+            <NavLink to="/projects" className="mr-5 hover:text-gray-900">
               Projects
-            </Link>
-            <Link href="/dashboard" className="mr-5 hover:text-gray-900">
+            </NavLink>
+            <NavLink to="/dashboard" className="mr-5 hover:text-gray-900">
               Dashboard
-            </Link>
+            </NavLink>
           </nav>
           <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
