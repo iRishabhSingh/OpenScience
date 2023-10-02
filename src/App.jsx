39826ed99ecb07a1e1dea6ac3b885b2b4678b36c
Ankita27Sharma <ankitasharma2743.gmail.com>
2023-10-02 11:41:12 +0530

    user role

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./pages/Signup/Signup";
+import Card from "./pages/Card";
 
 function App() {
   return (
+    <>
     <Router>
       <Switch>
         <Route path="/signup" component={Signup} />
+        <Route path="/selectrole" component={Card}/>
       </Switch>
     </Router>
+    <Card/>
+    </>
   );
 }
 
