cd39a40c3f3c99eaa4196ed7bcfdfc7094376803
Gzie3009 <108942030+Gzie3009@users.noreply.github.com>
2023-10-07 14:29:12 +0530

    chat ui made with video enabled

---
+++ src/pages/Project/MessageBox.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+const MessageBox = ({ content, position, time, type }) => {
+  return (
+    <div className={`chat chat-${position}`}>
+      <div
+        className={`chat-bubble w-1/3 flex flex-col ${
+          position === "end"
+            ? "bg-blue-500 text-white"
+            : " bg-slate-400 text-white"
+        } `}
+        style={{
+          wordWrap: "break-word", // Allow long words to wrap
+          overflowWrap: "break-word", // Control how unbreakable words are handled
+        }}
+      >
+        <p className={`text-md text-white`}>
+          {type === "link" ? (
+            <a className="hover:underline" target="_blank" href={content}>
+              {content}
+            </a>
+          ) : (
+            content
+          )}
+        </p>
+        <p className="text-[0.5rem]">{time}</p>
+      </div>
+    </div>
+  );
+};
+
+export default MessageBox;
